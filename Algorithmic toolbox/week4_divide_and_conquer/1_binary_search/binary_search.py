10c700a1fe98321da2d199bad51d88c031cdc62a
SaeedM75 <smoayedp@andrew.cmu.edu>
2021-10-16 14:07:21 -0400

    prob 3

--- Algorithmic toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ Algorithmic toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,6 +1,18 @@
 def binary_search(keys, query):
     # write your code here
 
+    low, high = 0, len(keys) - 1
+    while low < high:
+        mid = int((low + high) / 2)
+        if keys[mid] == query:
+            return mid
+        if keys[mid] > query:
+            high = mid - 1
+        if keys[mid] < query:
+            low  = mid + 1
+    return -1
+
+
 
 if __name__ == '__main__':
     num_keys = int(input())
@@ -10,6 +22,7 @@ if __name__ == '__main__':
     num_queries = int(input())
     input_queries = list(map(int, input().split()))
     assert len(input_queries) == num_queries
-
+    input_keys.sort()
     for q in input_queries:
+        # print(input_keys)
         print(binary_search(input_keys, q), end=' ')
--- Algorithmic toolbox/week4_divide_and_conquer/3_majority_element/majority_element.py
+++ Algorithmic toolbox/week4_divide_and_conquer/3_majority_element/majority_element.py
@@ -2,17 +2,33 @@
 import sys
 
 def get_majority_element(a, left, right):
-    if left == right:
+
+    if len(a) == 1:
+        return 1
+    mid = int(len(a ) / 2)
+
+    if a[left] == a[mid ]:
+        return 1
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
+
+    # if left == right:
+    #     return -1
+    # if left + 1 == right:
+    #     return a[left]
+    #
+
+
     #write your code here
-    return -1
+
+
+    # return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    a.sort()
+    if get_majority_element(a, 0, n - 1) != -1:
         print(1)
     else:
         print(0)
