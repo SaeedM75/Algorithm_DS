ce0d408e28310c811c67d16cc6465040d8cb26a8
SaeedM75 <smoayedp@andrew.cmu.edu>
2021-10-17 18:47:17 -0400

    prob 1 & 2

--- Algorithmic toolbox/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.py
+++ Algorithmic toolbox/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.py
@@ -1,6 +1,20 @@
 def binary_search(keys, query):
     # write your code here
-    pass
+
+    low, high = 0, len(keys) - 1
+    while low <= high:
+        mid = int((low + high) / 2)
+        if keys[mid] == query:
+            if mid > 0 and keys[mid - 1] == query:
+                high = mid - 1
+            else:
+                return mid
+        if keys[mid] > query:
+            high = mid - 1
+        if keys[mid] < query:
+            low  = mid + 1
+    return -1
+
 
 
 if __name__ == '__main__':
@@ -11,6 +25,7 @@ if __name__ == '__main__':
     num_queries = int(input())
     input_queries = list(map(int, input().split()))
     assert len(input_queries) == num_queries
-
+    input_keys.sort()
     for q in input_queries:
+        # print(input_keys)
         print(binary_search(input_keys, q), end=' ')
