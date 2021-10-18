58b0a33e1620978a020f8e224c57e28a89930a5e
SaeedM75 <smoayedp@andrew.cmu.edu>
2021-10-17 20:03:32 -0400

    p1

--- Algorithmic toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Algorithmic toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,8 +2,21 @@
 import sys
 
 def get_change(m):
+    min_num_coins = [0] * m
+    coins = [1, 3, 4]
+    for i in range(0, m ):
+        min_num_coins[i] = m + 1
+        for coin in coins:
+            if m >= coin:
+                num_coin = min_num_coins[i - coin] + 1
+                if num_coin < min_num_coins[i]:
+                    min_num_coins[i] = num_coin
+
+
     #write your code here
-    return m // 4
+
+
+    return min_num_coins[m - 1]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
